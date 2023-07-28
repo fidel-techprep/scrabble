ed9311b9087d97f9e1d4910965b42ff8e5c86f26
Fidel Leal <127764786+fidel-techprep@users.noreply.github.com>
2023-07-28 23:48:33 +0000

    clean up, add comments and better error handling

--- app.rb
+++ app.rb
@@ -4,75 +4,97 @@ require "http"
 require "ox"
 require "discordrb"
 
-
+# Fetch keys from the environment
 SCRABBLE_API_KEY = ENV.fetch("SCRABBLE_API_KEY")
 BOT_TOKEN = ENV.fetch("BOT_TOKEN")
 
-
+# Create Discord bot instance
 bot = Discordrb::Bot.new(token: BOT_TOKEN)
 
-# Event handler for the 'message' event
-bot.message do |event|
-  
-  # Check if the message is from the bot itself to avoid infinite loops
+# Event handler for the bot's 'message' event
+bot.message do | event |
+
+  ## Check if the message is from the bot itself
   next if event.author.bot_account?
-  server = bot.server(1133097855402381342)
   
+  ## Get the word from user's message
   word = event.message.content.downcase
   
+  ## Query API for word
   result = word_check(word)
 
+  ## Create embed for bot's response
   embed = Discordrb::Webhooks::Embed.new
+
+  ## Fetch message from event to reference in the reply
   message = event.message
   
-
-  if result == 0
+  ## Send the appropriate reply
+  if result == 0 
+    ### Word not found
     embed.color = 0xe0322f
     embed.title = "You are so creative!"
     embed.description = "But that is not a Scrabble word... "
     event.message.reply!("", tts: false, embed: embed)
-  else
+  elsif result == nil
+    ### Error case
+    embed.color = 0x000000
+    embed.title = "🙈 The dog ate my dictionary... 📖 🐕"
+    embed.description = "Sorry! Something went wrong with the search. Please try again later."
+    event.message.reply!("", tts: false, embed: embed)
+  else 
+    ### Word found
     embed.title = "It's a word!"
     embed.description = "#{word.capitalize} will earn you #{result} points."
     embed.color = 0x12b371 
-
     event.message.reply!("", tts: false, embed: embed)
   end
 end
 
 
-
-
-# Start the bot and wait for messages
+# Start the bot in a separate thread and listen for messages
 Thread.new { bot.run }
 
-
+# Helper method to query the API
 def word_check(word)
-  url = "https://www.wordgamedictionary.com/api/v1/references/scrabble/#{word}?key=#{SCRABBLE_API_KEY}"
+  ## Construct URL
+  url = "https://www.wordgamedictionary.com/api/v1/references/scrabble/#{word}?key=#{SCRABBLE_API_KEY}1"
 
+  ## Ensure input does not contain invalid characters
   if word.match(/\A[a-zA-Z]+\z/) 
     begin
       response = Ox.load(HTTP.get(url).to_s, mode: :hash)[:entry]
-    
-    rescue StandardError => e
+    rescue StandardError => e ### Handle parsing/network errors
       puts "<div>There was an error :( #{e.message}</div>"
-      response = {:scrabblescore => 0}
+      response = {:scrabblescore => nil}
     end
-    
+    ### Word found
     search_result = response[:scrabble] == "0" ? 0 : response[:scrabblescore] 
   else
+    ### Word not found
     search_result = 0
   end
 end
 
+# Render home route
 get("/") do
   erb(:home)
 end
 
+# Render results page
 get("/check") do
-  @word = params.fetch("word").downcase
-  @result = word_check(@word)
   
-  erb(:check)
+  ## Get user input
+  @word = params.fetch("word").downcase
+  
+  ## Query API for word
+  @result = word_check(@word)
 
+  ## Render error template if necessary
+  if @result == nil
+    erb(:error)
+  ## Render results page
+  else
+    erb(:check)
+  end
 end
